5197b07681dd4ef43cea3adeac3591f71151ac4b
jo su bin <a011096.aivle@gmail.com>
2022-01-22 11:31:31 +0900

    mypage 추가

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', config_views.home),
     path('weather/', include('weatherapp.urls')),
+    path('mypage/', include('mypage.urls')),
 ]
