83419cd36a8df071d79d38f3348ceff4422052c3
WooriHCho <woori.helena.cho@gmail.com>
2022-01-22 18:15:20 +0900

    kakao message api

---
+++ kakaomessage/Access_key.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+# Create your views here.
+
+#API 토큰 발급
+url = 'https://kauth.kakao.com/oauth/token'
+rest_api_key = '5d03e24af9d6c95a6f526e3308d8879d'
+redirect_uri = 'https://example.com/oauth'
+authorize_code = '-7sSp7Xxqw2QAew7-rctWJtZT4ycERTzQM_nylg8bX0OxFcgGegkA9jaVvPLEVOX_FrkxQo9dRoAAAF2qFCIsA'
+
+data = {
+    'grant_type':'authorization_code',
+    'client_id':rest_api_key,
+    'redirect_uri':redirect_uri,
+    'code': authorize_code,
+    }
+
+response = requests.post(url, data=data)
+tokens = response.json()
+print(tokens)
+
+# json 저장
+with open(r"C:\Users\User\Desktop\Python\kakao_code.json","w") as fp:
+    json.dump(tokens, fp)
+
+with open(r"C:\Users\User\Desktop\Python\kakao_code.json","r") as fp:
+    ts = json.load(fp)
+    
+print(ts)
+print(ts["access_token"])
