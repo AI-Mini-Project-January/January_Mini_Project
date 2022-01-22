d213d98b84bd555256ed411ef898dd45ee030b9b
youjin <english7212@gmail.com>
2022-01-22 10:57:31 +0900

    weather_api 테스트

---
+++ weatherapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'weatherapp'
+
+urlpatterns = [
+    path('get_weather/', views.get_weather),
+]
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+import json
+import datetime
 
-# Create your views here.
+def get_weather(request):
+    url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
+
+    service_key = 'Rty09EbsqEEgCQyDM03L%2F%2FhEwSnSIENiavOyVF3BsZwUSxzkFNKrJFgbXTSayi81l4WbTijUpuHbow5W%2FFwB4wRty09EbsqEEgCQyDM03L//hEwSnSIENiavOyVF3BsZwUSxzkFNKrJFgbXTSayi81l4WbTijUpuHbow5W/FwB4w=='
+
+    today = datetime.datetime.today()
+    base_date = today.strftime("%Y%m%d")
+    base_time = "0500"
+
+    nx = "62"
+    ny = "122"
+
+    params = "serviceKey=" + service_key + "&" +\
+        "dataType=json" + "&" +\
+        "base_date=" + base_date + "&" +\
+        "base_time=" + base_time + "&" +\
+        "nx=" + nx + "&" +\
+        "ny=" + ny
+
+    res = requests.get(url + params)
+
+    items = res.json().get('response').get('body').get('items')
+
+    # return HttpResponse(items)
+
