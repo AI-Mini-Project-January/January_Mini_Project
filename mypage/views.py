bd739cf65241c853129fb04611e520956043f0f3
jo su bin <a011096.aivle@gmail.com>
2022-01-24 14:05:16 +0900

    mypage app 삭제, mini1으로 통합

--- mypage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
-
-def member_info(request):
-    return render(request, 'mypage/member_info.html')
-
-def member_edit(request):
-    return render(request, 'mypage/member_edit.html')
